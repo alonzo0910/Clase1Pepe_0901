23d3c44bc7154af24801827e9930977b1c6b02cb
W4105 <W4105@lab.aca>
2020-01-09 18:51:19 -0500

    reseteo de contraseña y nuevas vistas

--- src/api/controllers/login-controller.ts
+++ src/api/controllers/login-controller.ts
@@ -16,6 +16,9 @@ class LoginController implements IControllerBase {
   public initRoutes() {
     this.router.get('/', sessionFalseMiddleware ,this.index)
     this.router.post('/access', sessionFalseMiddleware ,this.access)
+    this.router.get('/reset_password', sessionFalseMiddleware ,this.resetPassword)
+    this.router.get('/signac_in', sessionFalseMiddleware ,this.registrarUser)
+    this.router.get('/reset', sessionFalseMiddleware ,this.reset)
   }
 
   index = (req: Request, res: Response) => {
@@ -31,6 +34,36 @@ class LoginController implements IControllerBase {
       jss: loadJs([]), 
     }
     res.status(200).render('login/index', locals)
+  }
+
+  resetPassword = (req: Request, res: Response) => {
+    let locals = {
+      title: 'Olvidó su contraseña?',
+      constants: constants,
+      message_color: '',
+      message: '',
+      csss: loadCss([
+        'assets/css/styles',
+        'assets/css/login',
+      ]), 
+      jss: loadJs([]), 
+    }
+    res.status(200).render('login/reset', locals)
+  }
+
+  registrarUser = (req: Request, res: Response) => {
+    let locals = {
+      title: 'Olvidó su contraseña?',
+      constants: constants,
+      message_color: '',
+      message: '',
+      csss: loadCss([
+        'assets/css/styles',
+        'assets/css/login',
+      ]), 
+      jss: loadJs([]), 
+    }
+    res.status(200).render('login/registrar', locals)
   }
 
   access = (req: Request, res: Response) => {
@@ -51,6 +84,51 @@ class LoginController implements IControllerBase {
         jss: loadJs([]), 
       }
       res.status(200).render('login/index', locals)
+    }
+  }
+  reset = (req: Request, res: Response) => {
+    let mail = req.body.user
+    //ver si existe el correo en una lista de correos
+    let mails = [
+      'juan@aloe.ulima.edu.pe',
+      'pepe@aloe.ulima.edu.pe',
+      'pool@aloe.ulima.edu.pe'
+    ]
+    let exist = false
+    mail.array.forEach(function(temp) {
+      if(temp==mail){
+        exist=true
+      }      
+    });
+
+    if(exist){
+      let locals = {
+        title: 'Bienvenido',
+        constants: constants,
+        message_color: 'text-success',
+        message: 'Se envió al correo su contraseña',
+        csss: loadCss([
+          'assets/css/styles',
+          'assets/css/login',
+        ]), 
+        jss: loadJs([]), 
+      }
+      res.status(200).render('login/index', locals)
+    
+
+    }else{
+      let locals = {
+        title: 'Bienvenido',
+        constants: constants,
+        message_color: 'text-danger',
+        message: 'Correo no registrado',
+        csss: loadCss([
+          'assets/css/styles',
+          'assets/css/login',
+        ]), 
+        jss: loadJs([]), 
+      }
+      res.status(200).render('login/reset', locals)
     }
   }
 }
